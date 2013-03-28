b9386e199ebf45a4167d02fe087f824cbee92843
Tann <tanneguy.dulong@gmail.com>
2013-03-28 17:25:01 +0100

    synchro pour travail @home
    Attention le count oublie les STOP

--- count_ngram.hs
+++ count_ngram.hs
@@ -45,21 +45,16 @@ type CntReg = (Int, Map [String] Int, [String])
 memCount ::  CntReg  -> String -> CntReg
 memCount (n, m, w) x =
     n `seq` m `seq` w `seq` 
-    let 
-        y = drop 1 (w++[x]) 
-        (x1, x2, x3) = ([x], tail y, y)
-    in 
-    if (length y < n)
-    then (n, m, y)
-    else let
-            m1 = condInsert x1 m
+    let y             = drop 1 (w++[x]) 
+        (x1, x2, x3)  = ([x], tail y, y)
+    in let  m1 = condInsert x1 m
             m2 = condInsert x2 m1
             m3 = condInsert x3 m2
-         in (n, m3, y)
+       in (n, m3, y)
 
 condInsert :: [String] -> Map [String] Int -> Map [String] Int 
 condInsert x m =  
-    if ("" `elem` x) 
+    if head x == "STOP"
     then m
     else M.insertWith (+) x 1 m
 
@@ -68,7 +63,8 @@ condInsert x m =
 -- tailz : liste des listes de mots de chaque ligne (excepté le 1er)  ou "\n" si la ligne vaut "\n"
 -- tailz "toto titi tata" == ["titi", "tata"]
 -- tailz "\n" == [""]
-wordz "" = ["", ""]
+-- wordz "" = ["", "STOP"]
+wordz "" = ["", "STOP"]
 wordz s  = words s
 premz = P.map (head . wordz) 
 tailz = P.map (tail . wordz)
